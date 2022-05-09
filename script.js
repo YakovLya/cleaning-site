3d0c88aa023e20383a7757d6f0fd61650bad89ad
YakovLya <yakaulyahovich@gmail.com>
2022-05-09 17:11:49 +0300

    init: uploade files

---
+++ script.js
@@ -0,0 +1,133 @@
+const cleanTypes = [
+  '<h3 class="choice__title">Basic clean</h3>\n<p class="choice__desc">Basic dusting, mopping and vacuuming of the entire apartment. Rates include 1 bathroom.</p>\n<p class="choice__desc">Weekend minimum rate is $150.</p>\n<ul class="choice__prop-list">\n<li class="choice__prop">STUDIO – $100</li>\n<li class="choice__prop">1-BEDROOM – $110</li>\n<li class="choice__prop">2-BEDROOM – $120</li>\n<li class="choice__prop">3-BEDROOM – $130</li>\n</ul>\n<p class="choice__desc">Basic cleaning is only for apartments that are in good condition and have been deep cleaned by our Cleaning Team. If your home is clean and doesn’t need to be scrubbed then you can get pre-qualified for basic cleaning by sending us photos of your home.</p>\n<h5 class="choice__subtitle">In all rooms:</h5>\n<ul class="choice__prop-list">\n<li class="choice__prop">Dusting/polishing/vacuuming the furniture and floors. We will not move the furniture but will reach under it to clean there.</li>\n<li class="choice__prop">Dry dusting the baseboards (deep cleaning baseboards is $40 an hour!)</li>\n<li class="choice__prop">Mopping the floors</li>\n<li class="choice__prop">Taking out the trash and replacing trash bags/liners.</li>\n<li class="choice__prop">Making up the beds. Please leave clean bedding on the bed if you want us to change sheets.</li>\n</ul>\n<h5 class="choice__subtitle">In bathroom:</h5>\n<ul class="choice__prop-list">\n<li class="choice__prop">Cleaning and sanitizing the bathroom (scrubbing walls, tub, toilet and sink).</li>\n<li class="choice__prop">Polishing bathroom faucets and mirrors.</li>\n</ul>\n<h5 class="choice__subtitle">In kitchen:</h5>\n<ul class="choice__prop-list">\n<li class="choice__prop">Doing dishes by hand or running the machine. Please let us know if you have more than one sink or machine full of dishes. Extra charge will apply. Please do not leave expensive or extremely fragile dishes for us to clean.</li>\n<li class="choice__prop">Cleaning inside the microwave</li>\n<li class="choice__prop">Wiping down and polishing the countertops and stove top</li>\n</ul>\n<p class="choise__desc choise__small">  Rate does NOT include cleaning the blinds/shutters, fans, organizing inside the cabinets-closets-drawers, washing the walls, laundry, scrubbing baseboards, windows etc. Please notify us at least 2 days prior to your appointment if you\'d like to add any extras so we can make changes to our schedule and adjust your rate.</p>',
+  '<h3 class="choice__title">Deep clean</h3>\n<p class="choice__desc">For homes that need some serious scrubbing-dusting. Rates include 1 bathroom!</p>\n<ul class="choice__prop-list">\n<li class="choice__prop">STUDIO – $160</li>\n<li class="choice__prop">1-BEDROOM – $180</li>\n<li class="choice__prop">2-BEDROOM – $200</li>\n<li class="choice__prop">3-BEDROOM – $230</li>\n</ul>\n<p class="choice__desc">Rates include one bathroom and are for one-level apartments. If your home is bigger than 3 bedrooms and 1 bathroom then we\'ll need to get a detailed description and photos of your home so we can give you the flat rate.   If your home has not been cleaned in a long time and there is lots of clutter or dirt/dust buildup then you may not qualify for a deep clean. Please send pictures if you\'d like to be sure you qualify for deep clean. If we show up and see that you are not qualified for a deep clean then we may not be able to complete the clean as promised.</p>\n<h5 class="choice__subtitle">In all rooms:</h5>\n<ul class="choice__prop-list">\n<li class="choice__prop">Dusting/polishing/vacuuming the floors. We will move the furniture to clean under it as long as it\'s safe to do so without hurting ourselves or scratching floors.</li>\n<li class="choice__prop">Mopping the floors (with Swiffer and wet cloth. Please provide your own mop if you want us to use anything else).</li>\n<li class="choice__prop">Washing the windows from inside (let us know if they are over-size!).</li>\n<li class="choice__prop">Cleaning the baseboards</li>\n<li class="choice__prop">Making the beds in bedrooms. Please leave clean bedding on the bed if you want sheets changed.</li>\n</ul>\n<h5 class="choice__subtitle">In bathroom:</h5>\n<ul class="choice__prop-list">\n<li class="choice__prop">Cleaning and sanitizing the bathroom (scrubbing walls, tub, toilet and sink).</li>\n<li class="choice__prop">Polishing bathroom faucets and mirrors.</li>\n</ul>\n<h5 class="choice__subtitle">In kitchen:</h5>\n<ul class="choice__prop-list">\n<li class="choice__prop">Doing dishes by hand or running the machine. Please let us know if you have more than one sink or machine full of dishes. Extra charge will apply. Please do not leave expensive or extremely fragile dishes for us to clean.</li>\n<li class="choice__prop">Cleaning inside the microwave</li>\n<li class="choice__prop">Wiping down and polishing the countertops and stove top</li>\n<li class="choice__prop">Scrubbing and Polishing kitchen cabinet doors</li>\n</ul>\n<p class="choise__desc choise__small">  Rate does NOT include cleaning the blinds/shutters, fans, organizing inside the cabinets/closets/drawers, washing the walls, laundry or cleaning inside the oven.  </p>',
+  '<h3 class="choice__title">Move-In/Out</h3>\n<p class="choice__desc">Deep clean of an empty apartment. Rates include 1 bathroom.</p>\n<ul class="choice__prop-list">\n<li class="choice__prop">STUDIO – $160</li>\n<li class="choice__prop">1-BEDROOM – $180</li><li class="choice__prop">2-BEDROOM – $200</li>\n<li class="choice__prop">3-BEDROOM – $230</li>\n</ul>\n<p class="choice__desc">Apartment needs to be empty!   Please let us know if your home has been painted or renovated, there\'s an extra charge for post-reno cleans.  </p>\n<h5 class="choice__subtitle">In all rooms:</h5>\n<ul class="choice__prop-list">\n<li class="choice__prop">Dusting/polishing/vacuuming the floors.</li>\n<li class="choice__prop">Mopping the floors (with Swiffer and wet cloth. Please provide your own mop if you want us to use anything else).</li>\n<li class="choice__prop">Cleaning inside all closets, cabinets and drawers</li>\n<li class="choice__prop">Washing the windows from inside (let us know if they are over-size!).</li>\n<li class="choice__prop">Cleaning the baseboards</li>\n<li class="choice__prop">Taking out the trash. Please let us know if there are certain items you do not want thrown away). Please be sure to let us know if you have a large amount of trash you need removed from the apartment. We are only planning to remove 1-2 bags of trash.</li>\n</ul>\n<h5 class="choice__subtitle">In bathroom:</h5>\n<ul class="choice__prop-list">\n<li class="choice__prop">Cleaning and sanitizing the bathroom (scrubbing walls, tub, toilet and sink).</li>\n<li class="choice__prop">Polishing bathroom faucets and mirrors.</li>\n</ul>\n<h5 class="choice__subtitle">In kitchen:</h5>\n<ul class="choice__prop-list">\n<li class="choice__prop">Cleaning inside the fridge, oven and microwave</li>\n<li class="choice__prop">Polishing the counter top and stove top</li>\n<li class="choice__prop">Scrubbing and Polishing kitchen cabinet doors</li>\n</ul>\n<p class="choise__desc choise__small">  Rate does not include washing the windows from the outside, cleaning the tops of kitchen cabinets, detail cleaning of ACs, doors or light switches, scrubbing the floors under big appliances. All that can be added on for extra charge.</p>',
+  '<h3 class="choice__title">Filth clean</h3>\n<p class="choice__desc">For homes that are very messy and filthy. These homes do not qualify for a deep clean.</p>\n<ul class="choice__prop-list">\n<li class="choice__prop">AN HOUR – $75</li>\n<p class="choice__desc">Be honest: if your apartment has never been cleaned, you haven\'t taken out the trash in months, the dishes are piling up or neighbors are complaining about the smell then a basic or deep clean will not be enough.</p>\n</ul>\n<p class="choice__desc">Don\'t order a deep clean or basic clean if your home really needs a filth clean.  Please warn us about the current state of your apartment because only a handful of Wizards are qualified to work in your home.</p>\n</ul>\n<p class="choice__desc">We reserve the right to walk out if you have ordered a basic/deep clean for an apartment in need of a filth clean. If you\'re not sure then please send us pictures. For these cleans we only send Wizards that have agreed to accept filth clean jobs and have enough experience.</p>\n</ul>\n<p class="choice__desc">Filth cleaning is for extremely dirty and messy apartments. Rate would be $75 an hour per person and you likely need more than 1 person. They can bring all the equipment they need, including a vacuum cleaner and paper towels. You are welcome to set a maximum time you allow them to stay to keep the service within your budget.</p>',
+  '<div class="choice__right">\n<h3 class="choice__title">Details</h3>\n<p class="choice__desc">For people who want to dictate exactly what they want done.</p>\n<ul class="choice__prop-list">\n<li class="choice__prop">AN HOUR – $50</li>\n<p class="choice__desc">If you feel like deep clean tasks are not enough for you but filth clean is too much, then "Details" is the service for you! You can send us pictures and describe your needs as specifically as possible so we can give you an estimate of how many hours we\'ll need in your home and we\'ll try our best to get it done within those hours to respect your budget.</p>\n\n<p class="choice__desc">You are also welcome to book just the "detailing" of the bathroom, kitchen or any other room in your home but there\'s a two hour minimum for this service.</p>\n<p class="choice__desc choise__desc choise__small">Rate: $50 an hour per person (you can book more than one Wizard at a time to get the job done faster).</p>\n',
+  '<div class="choice__right">\n<h3 class="choice__title">Organizing</h3>\n<p class="choice__desc">$50 an hour to get your things in order</p>\n<ul class="choice__prop-list">\n<li class="choice__prop">AN HOUR – $50</li>\n<p class="choice__desc">Closets-Drawers-Clothes-Papers-Cupboards: all of that can be arranged and made neat. You can add a few hours of organizing to your cleaning service ($50 an hour)</p>\n</ul>',
+  '<div class="choice__right">\n<h3 class="choice__title">Post construction</h3>\n<p class="choice__desc">For empty homes to get cleaned after renovations, construction or painting. Rates include 1 bathroom.</p>\n<ul class="choice__prop-list">\n<li class="choice__prop">STUDIO – $200</li>\n<li class="choice__prop">1-BEDROOM – $230</li>\n<li class="choice__prop">2-BEDROOM – $250</li>\n<li class="choice__prop">3-BEDROOM – $260</li>\n</ul>\n<p class="choice__desc">If you book your cleaning, ask for a very realistic date from your construction company and ask them to alert you of any delays so you can reschedule your cleaning appointment accordingly.   In the event that you do want us to clean alongside any other trades we\'ll convert to an hourly rate, $40 an hour per person. But again, we strongly advise against this.   If your home is bigger than 2 bedrooms-2 bathrooms or 1500 sf then we\'ll need more details about your home in order to give you the flat rate.   <h5 class="choice__subtitle">Apartment that is not empty:</h5> $40 an hour per person. We are sorry but we cannot give a flat rate for this type of appointment. In some cases all the knick-knacks and furniture need to be individually dusted several times and that can take a long time so we need to charge hourly for this type of service.</p>\n<h5 class="choice__subtitle">In all rooms:</h5>\n<ul class="choice__prop-list">\n<li class="choice__prop">Dusting/polishing/vacuuming the floors and walls.</li>\n<li class="choice__prop">Mopping the floors (with Swiffer and wet cloth. Please provide your own mop if you want us to use anything else).</li>\n<li class="choice__prop">Cleaning inside all closets, cabinets and drawers</li>\n<li class="choice__prop">Washing the windows from inside (let us know if they are over-size!).</li>\n<li class="choice__prop">Cleaning the baseboards</li>\n<li class="choice__prop">Taking out the trash. Please let us know if there are certain items you do not want thrown away). Please be sure to let us know if you have a large amount of trash you need removed from the apartment. We are only planning to remove 1-2 bags of trash.</li>\n</ul>\n<h5 class="choice__subtitle">In bathroom:</h5>\n<ul class="choice__prop-list">\n<li class="choice__prop">Cleaning and sanitizing the bathroom (scrubbing walls, tub, toilet and sink).</li>\n<li class="choice__prop">Polishing bathroom faucets and mirrors.</li>\n</ul>\n<h5 class="choice__subtitle">In kitchen:</h5>\n<ul class="choice__prop-list">\n<li class="choice__prop">Cleaning inside the fridge, oven and microwave</li>\n<li class="choice__prop">Polishing the countertop and stove top</li>\n<li class="choice__prop">Scrubbing and Polishing kitchen cabinet doors</li>\n</ul>\n<p class="choise__desc choise__small">  Rate does not include washing the windows from the outside, cleaning the tops of kitchen cabinets, detailed cleaning of ACs, doors or light switches, scrubbing the floors under big appliances. All that can be added on for extra charge.  </p></div>',
+  '<div class="choice__right">\n<h3 class="choice__title">Recurring</h3>\n<p class="choice__desc">Weekly or Bi-weekly Wizard visits. Rates include 1 bathroom.</p>\n<p class="choice__desc">Weekend minimum rate is $150.</p>\n<ul class="choice__prop-list">\n<li class="choice__prop">STUDIO – $80</li>\n<li class="choice__prop">1-BEDROOM – $90</li>\n<li class="choice__prop">2-BEDROOM – $100</li>\n<li class="choice__prop">3-BEDROOM – $120</li>\n</ul>\n<p class="choice__desc">Monthly rates are as follows:   Studio: $100   1-bedroom: $110   2-bedroom: $120   3-bedroom: $140   </p>\n<h5 class="choice__subtitle">In all rooms:</h5>\n<ul class="choice__prop-list">\n<li class="choice__prop">Dusting/polishing/vacuuming the furniture and floors. We will not move the furniture but will reach under it to clean there.</li>\n<li class="choice__prop">Dry dusting the baseboards</li>\n<li class="choice__prop">Mopping the floors</li>\n<li class="choice__prop">Taking out the trash and replacing trash bags/liners.</li>\n<li class="choice__prop">Making up the beds. Please leave clean bedding on bed if you want us to change sheets.</li>\n</ul>\n<h5 class="choice__subtitle">In bathroom:</h5>\n<ul class="choice__prop-list">\n<li class="choice__prop">Cleaning and sanitizing the bathroom (scrubbing walls, tub, toilet and sink).</li>\n<li class="choice__prop">Polishing bathroom faucets and mirrors.</li>\n</ul>\n<h5 class="choice__subtitle">In kitchen:</h5>\n<ul class="choice__prop-list">\n<li class="choice__prop">Doing dishes by hand or running the machine. Please let us know if you have more than one sink or machine full of dishes. Extra charge will apply. Please do not leave expensive or extremely fragile dishes for us to clean.</li>\n<li class="choice__prop">Cleaning inside the microwave</li>\n<li class="choice__prop">Wiping down and polishing the counter tops and stove top</li>\n</ul>\n<p class="choise__desc choise__small">  Rate does NOT include cleaning inside the fridge or oven, blinds/shutters, windows, fans, organizing inside the cabinets-closets-drawers, washing the walls, laundry, scrubbing baseboards, windows etc.   However, your Wizard will do these things when he/she has time left over from other tasks.  </p></div>'
+]
+
+const cleanPrice = [
+  100,
+  160,
+  160,
+  75,
+  50,
+  50,
+  200,
+  80
+]
+
+
+class Menu{
+  animLength = 400;
+
+  constructor(){
+    this.menuBtnWrapper = document.querySelector('.menu-btn');
+    this.menuBtn = document.querySelector('.menu-btn__btn');
+    this.menuElem = document.querySelector('.menu-panel');
+    this.menuItems = document.querySelectorAll('.menu-panel__item');
+
+    this.menuBtn.addEventListener('click', () => {
+      this.menuToggle();
+    });
+
+    this.menuItems[0].addEventListener('click', () => {
+      document.querySelector('.slider').scrollIntoView();
+      this.menuToggle();
+    });
+
+    this.menuItems[1].addEventListener('click', () => {
+      document.querySelector('.qualities').scrollIntoView();
+      this.menuToggle();
+    });
+  }
+
+  menuToggle(){
+    this.menuBtn.disabled = true;
+    this.menuBtnWrapper.classList.toggle('pressed');
+    if (this.menuElem.classList.contains('hide')){
+      this.menuElem.classList.toggle('hide');
+    } else {
+      this.menuElem.classList.toggle('menu-up');
+      setTimeout(() => {
+        this.menuElem.classList.toggle('hide');
+        this.menuElem.classList.toggle('menu-up');
+      }, this.animLength);
+    }
+
+    setTimeout(() => {
+      this.menuBtn.disabled = false;
+    }, this.animLength);
+  }
+}
+
+class Choise{
+  constructor(){
+    this.book = document.querySelector('.fast-btn');
+    this.overlay = document.querySelector('.choice__overlay');
+    this.choise = document.querySelector('.choice');
+    this.choiseClose = document.querySelector('.choice__close');
+    this.choiseDesc = document.querySelector('.choice__right');
+    this.choisePrice = document.querySelector('.choice__price');
+    this.choiseSelect = document.querySelector('select');
+    this.changeClean(0);
+    this.choiseSelect.addEventListener('change', () => {
+      this.changeClean(this.choiseSelect.value - 1);
+    });
+
+    this.cards = document.querySelectorAll('.card__link');
+    this.cards.forEach((item, index) => {
+      item.addEventListener('click', () => {
+        this.changeClean(index);
+        this.changeState();
+      });
+    });
+
+    this.book.addEventListener('click', () => {
+      this.changeState();
+    });
+
+    this.choiseClose.addEventListener('click', () => {
+      this.changeState();
+    });
+
+    this.overlay.addEventListener('click', () => {
+      this.changeState();
+    })
+  }
+
+  changeState(){
+    if (this.choise.classList.contains('hide')){
+      this.overlay.classList.toggle('hide');
+      this.choise.classList.toggle('hide');
+      setTimeout(() => {
+        this.choise.style.left = '50%';
+      }, 50)
+    } else {
+      this.choise.style.left = '150%';
+      setTimeout(() => {
+        this.overlay.classList.toggle('hide');
+        this.choise.classList.toggle('hide');
+      }, 500);
+    }
+  }
+
+  changeClean(index){
+    this.choiseSelect.selectedIndex = index;
+    this.choiseDesc.innerHTML = cleanTypes[index];
+    this.choisePrice.innerText = cleanPrice[index];
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  new Menu();
+  new Choise();
+  const slider = new ChiefSlider('.slider', {
+    loop: true,
+    autoplay: true,
+  });
+});
+
